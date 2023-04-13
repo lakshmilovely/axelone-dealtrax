7392d9ef87351dde13856b96d430c77b80364977
lakshmilovely <105770396+lakshmilovely@users.noreply.github.com>
2023-04-13 12:23:06 +0530

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  // apiUrl: 'https://traxapi.swickard.com/api/',
+  // actionHubUrl: 'https://devtraxapi.axelautomotive.com/action',
+  // messageHubUrl: 'https://devtraxapi.axelautomotive.com/message',
+  // apitask: 'https://taskapi.swickard.com/api/',
+  // thumbUrl:
+  //   'https://axeloneapi.axelautomotive.com/api/v1/resources/profilepics/thumbs/',
+  // imgURL: 'https://axeloneapi.axelautomotive.com/api/v1/resources/profilepics/',
+
+  apiUrl: 'https://devtraxapi.axelautomotive.com/api/',
+  actionHubUrl: 'https://devtraxapi.axelautomotive.com/action',
+  messageHubUrl: 'https://devtraxapi.axelautomotive.com/message',
+  apitask: 'https://devtaskapi.axelautomotive.com/api/',
+  thumbUrl:
+    'https://devaxeloneapi.axelautomotive.com/api/v1/resources/profilepics/thumbs/',
+  imgURL:
+    'https://devaxeloneapi.axelautomotive.com/api/v1/resources/profilepics/',
+};
---
+++ environments/environment.ts
@@ -0,0 +1,34 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  apiUrl:"https://devtraxapi.axelautomotive.com/api/",
+  actionHubUrl:"https://devtraxapi.axelautomotive.com/action",
+   messageHubUrl:"https://devtraxapi.axelautomotive.com/message",
+   apitask:"https://devtaskapi.axelautomotive.com/api/",
+   thumbUrl:"https://devaxeloneapi.axelautomotive.com/api/v1/resources/profilepics/thumbs/",
+   imgURL:"https://devaxeloneapi.axelautomotive.com/api/v1/resources/profilepics/",
+
+  // apiUrl: 'https://traxapi.swickard.com/api/',
+  // actionHubUrl: 'https://devtraxapi.axelautomotive.com/action',
+  // messageHubUrl: 'https://devtraxapi.axelautomotive.com/message',
+  // apitask: 'https://taskapi.swickard.com/api/',
+  // thumbUrl:
+  //   'https://axeloneapi.axelautomotive.com/api/v1/resources/profilepics/thumbs/',
+  // imgURL: 'https://axeloneapi.axelautomotive.com/api/v1/resources/profilepics/',
+
+  // apiUrl: 'https://localhost:9999/api/',
+  // actionHubUrl: 'https://localhost:9999/action',
+  // messageHubUrl: 'https://localhost:9999/message',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
